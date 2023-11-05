3389d3eed55889c923881c064e4d12978886fdf5
adam-fabis <adamfabis96@gmail.com>
2023-11-05 21:21:01 +0100

    project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        softBlue: "	#5368df",
+        softRed: "#fa5757",
+        grayishBlue: "#9194a1",
+        veryDarkBlue: "#252b46",
+      },
+      fontFamily: {
+        sans: ["Rubik", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
